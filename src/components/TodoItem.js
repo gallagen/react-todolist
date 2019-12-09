7041fadc93d98d4195bf72d68051659326df7376
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-09 22:08:28 +0300

    Component conversion

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+
+      <input type="checkbox" />
+      <p>Text here</p>
+
+    </div>
+  );
+}
+
+export default TodoItem;
