ed8b87596d81e87a4ce99fb7480fc26ee5f1a41c
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-13 13:33:55 +0300

    Creating component files, working with properties

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import TodoItem from "./components/TodoItem"
+import todosData from "./components/todosData"
 
 function App() {
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+
   return (
     <div className="todo-list">
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
-    <TodoItem />
+      {todoItems}
       </div>
   );
 }
